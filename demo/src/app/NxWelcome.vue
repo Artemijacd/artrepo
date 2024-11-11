8d04b276af5375ccdde047eb558c1b4721ef9683
Artemiy Yakovlev <artemiy.yakovlev@kmslh.com>
2024-11-11 15:47:54 +0300

    add monorepo and npm-package

---
+++ demo/src/app/NxWelcome.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import {core} from '@core-main';
+import {utils} from '@core-utils';
+import { helloNPM } from "my-npm-package";
+defineProps<{
+  title: string;
+}>();
+</script>
+
+<template>
+  <div class="wrapper">
+    <h1>Hello world!</h1>
+    <p>{{ core() }}</p>
+    <p>{{ utils() }}</p>
+    <p>{{ helloNPM() }}</p>
+  </div>
+</template>
---
+++ packages/core/src/lib/core.ts
@@ -0,0 +1,3 @@
+export function core(): string {
+  return 'hi from core!';
+}
